5bd1650b198f2fdbff55a4bbd9e9f476426306a3
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 19:27:51 +0530

    Add files via upload

---
+++ Quantifier Operators/Any/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators___Quantifier_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] IntArray = { 11, 22, 33, 44, 55 };
+            //Using Method Syntax
+            var ResultMS = IntArray.Any();
+            //Using Query Syntax
+            var ResultQS = (from num in IntArray
+                            select num).Any();
+            Console.WriteLine("Is there any element in the collection : " + ResultMS);
+            Console.ReadKey();
+        }
+    }
+}
