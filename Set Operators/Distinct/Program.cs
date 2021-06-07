43702eecffcce718038409cb7802a292ff701880
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 19:17:09 +0530

    Add files via upload

---
+++ Set Operators/Distinct/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Operators___Set_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intCollection = new List<int>()
+            {
+                1,2,3,2,3,4,4,5,6,3,4,5
+            };
+            //Using Method Syntax
+            var MS = intCollection.Distinct();
+            //Using Query Syntax
+            var QS = (from num in intCollection
+                      select num).Distinct();
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
