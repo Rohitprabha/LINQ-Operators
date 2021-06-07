cf4ce6d94ce508e20b78338cdbf8e0bdc33e6926
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 19:26:15 +0530

    Add files via upload

---
+++ Quantifier Operators/All/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators___Quantifier_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] IntArray = { 11, 22, 33, 44, 55 };
+            var Result = IntArray.All(x => x > 10);
+            Console.WriteLine("Is All Numbers are greater than 10 : " + Result);
+            Console.ReadKey();
+        }
+    }
+}
