14e8b37235425d42c1c314929bbb6718d8254c1f
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 19:53:46 +0530

    Add files via upload

---
+++ Element Operators/DefaultIfEmpty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ_Operators___Element_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { };
+            IEnumerable<int> result = numbers.DefaultIfEmpty();
+            foreach (int num in result)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
