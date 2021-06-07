641175906a93ba7466a54fd0aaec1ccf03735aa8
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 19:20:49 +0530

    Add files via upload

---
+++ Set Operators/Intersect/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Operators___Set_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> dataSource1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> dataSource2 = new List<int>() { 1, 3, 5, 8, 9, 10 };
+            //Method Syntax
+            var MS = dataSource1.Intersect(dataSource2).ToList();
+            //Query Syntax
+            var QS = (from num in dataSource1
+                      select num)
+                      .Intersect(dataSource2).ToList();
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
