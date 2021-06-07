e99d125292cc59286484a29dd0b176157098e9d0
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 20:07:59 +0530

    Add files via upload

---
+++ Aggregation Operators/Sum,Max,Min,Average,Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators____Aggregation_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 10, 30, 50, 40, 60, 20, 70, 90, 80, 100 };
+            int MSTotal = intNumbers.Sum();
+            Console.WriteLine("Sum = " + MSTotal);
+
+            int MSLergestNumber = intNumbers.Max();
+            Console.WriteLine("Largest Number = " + MSLergestNumber);
+
+            int MSLowestNumber = intNumbers.Min();
+            Console.WriteLine("Lowest Number = " + MSLowestNumber);
+
+            var MSAverageValue = intNumbers.Average();
+            Console.WriteLine("Average Value = " + MSAverageValue);
+
+            int MSCount = intNumbers.Count();
+            Console.WriteLine("No of Elements = " + MSCount);
+            Console.ReadKey();
+        }
+    }
+}
