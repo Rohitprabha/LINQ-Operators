c894c1ff698c04d1bbc2c54aaf7a7344b7afaf64
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 20:10:54 +0530

    Add files via upload

---
+++ Aggregation Operators/Aggregate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators____Aggregation_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] skills = { "C#.NET", "MVC", "WCF", "SQL", "LINQ", "ASP.NET" };
+            string result = skills.Aggregate((s1, s2) => s1 + ", " + s2);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
