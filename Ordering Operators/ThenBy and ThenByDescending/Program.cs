3c662c1787e8e451d2eef0347d422a6d4c3872fb
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 17:04:08 +0530

    Add files via upload

---
+++ Ordering Operators/ThenBy and ThenByDescending/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators___Ordering_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method Syntax
+            var MS = Student.GetAllStudents()
+                              .OrderBy(x => x.FirstName)
+                              .ThenBy(y => y.LastName)
+                              .ToList();
+
+            foreach (var student in MS)
+            {
+                Console.WriteLine("First Name :" + student.FirstName + ", Last Name : " + student.LastName);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ThenByDescending");
+            var MS2 = Student.GetAllStudents()
+                              .OrderBy(x => x.FirstName)
+                              .ThenByDescending(y => y.LastName)
+                              .ToList();
+
+            foreach (var student in MS2)
+            {
+                Console.WriteLine("First Name :" + student.FirstName + ", Last Name : " + student.LastName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
