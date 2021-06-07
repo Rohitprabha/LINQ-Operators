730f425db816523244581dff1cd8e6a035cffc31
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 20:13:53 +0530

    Add files via upload

---
+++ Generation Operators/Empty/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Operators____Generation_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emptyCollection1 = Enumerable.Empty<string>();
+            var emptyCollection2 = Enumerable.Empty<Student>();
+            Console.WriteLine("Count: {0} ", emptyCollection1.Count());
+            Console.WriteLine("Type: {0} ", emptyCollection1.GetType().Name);
+            Console.WriteLine("Count: {0} ", emptyCollection2.Count());
+            Console.WriteLine("Type: {0} ", emptyCollection2.GetType().Name);
+            Console.ReadKey();
+        }
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
