f1f0810b909cc95df8af49cc4d8de16d3b83ae8b
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 20:18:04 +0530

    Add files via upload

---
+++ Generation Operators/Repeat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Operators____Generation_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> repeatStrings = Enumerable.Repeat("Hai There!", 10);
+            foreach (String str in repeatStrings)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
