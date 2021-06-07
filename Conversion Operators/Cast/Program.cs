4ad7699ae6ae664f0c50e4f3f8cee3752ce290b8
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-07 20:03:08 +0530

    Add files via upload

---
+++ Conversion Operators/Cast/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Operators____Conversion_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList
+            {
+                10,
+                20,
+                30,
+                "50"
+            };
+            IEnumerable<int> result = list.Cast<int>();
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
