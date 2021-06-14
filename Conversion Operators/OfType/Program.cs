db0a9e316e02edbde1326b16712cef2ce396f86f
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-14 09:53:56 +0530

    Add files via upload

---
+++ Conversion Operators/OfType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace LINQ_Operators____Conversion_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(2);
+            al.Add(4);
+            al.Add(6);
+            al.Add(8);
+            al.Add("9");
+            IEnumerable<int> numbers = al.OfType<int>();
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
