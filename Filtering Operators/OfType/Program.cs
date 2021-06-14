5827470ccc7bacb8fc91c5220254c05be1ec8ca5
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-14 14:11:04 +0530

    Add files via upload

---
+++ Filtering Operators/OfType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Operators___Filtering_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> dataSource = new List<object>()
+            {
+                "Tom", "Mary", 50, "Prince", "Jack", 10, 20, 30, 40, "James"
+            };
+
+            List<int> intData = dataSource.OfType<int>().ToList();
+
+            foreach (int number in intData)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
